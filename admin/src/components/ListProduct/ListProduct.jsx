a81f0b578f7a86c74ce5be0b8575112de186335f
01sidharth <sidharthkv08@gmail.com>
2024-03-06 00:11:03 +0530

    Admin page updates added

---
+++ admin/src/components/ListProduct/ListProduct.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react'
+import "./ListProduct.css"
+import cross_icon from '../../assets/cross_icon.png'
+const ListProduct = () => {
+  const [allproducts,setAllproducts]=useState([]);
+
+  const fetechInfo =async()=>{
+    await fetch('http://localhost:4000/allproducts')
+    .then((resp)=>resp.json())
+    .then((data)=>{setAllproducts(data)});
+  }
+
+  useEffect(()=>{
+    fetechInfo();
+  },[])
+
+  const remove_product= async()=>{
+    await fetch('http://localhost:4000/removeproduct',{
+      method:'POST',
+      headers:{
+        Accept:'application/json',
+        'Content-Type':'application/json',
+      },
+      body:JSON.stringify({id:id})
+    })
+  }
+  return (
+    <div className='list-product'>
+      <h1>All Products List</h1>
+      <div className="listproduct-format-main">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Old Price</p>
+        <p>New Price</p>
+        <p>Category</p>
+        <p>Remove</p>
+      </div>
+      <div className="listproduct-allproducts">
+        <hr />
+        {allproducts.map((product,index)=>{
+          return<><div  key={index} className='listproduct-format-main listproduct-format'>
+              <img src={product.image} alt="" className="listproduct-product-icon" />
+              <p>{product.name}</p>
+              <p>${product.old_price}</p>
+              <p>${product.new_price}</p>
+              <p>{product.category}</p>
+              <img  className='listproduct-remove-icon' src={cross_icon} alt="" />
+          </div>
+          <hr /></> 
+        })}
+
+      </div>
+    </div>
+  )
+}
+
+export default ListProduct
--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -6,16 +6,16 @@ import list_product_icon from '../../assets/Product_list_icon.svg'
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-        <Link to={'/addproduct'} style={{textDecoration:"none"}}></Link>
+        <Link to={'/addproduct'} style={{textDecoration:"none"}}>
         <div className='sidebar-item'>
             <img src={addd_product_icon} alt="" />
             <p>Add Product</p>
-        </div>
-        <Link to={'/listproduct'} style={{textDecoration:"none"}}></Link>
+        </div></Link>
+        <Link to={'/listproduct'} style={{textDecoration:"none"}}>
         <div className='sidebar-item'>
             <img src={list_product_icon} alt="" />
             <p>Product List</p>
-        </div>
+        </div></Link>
     </div>
   )
 }
